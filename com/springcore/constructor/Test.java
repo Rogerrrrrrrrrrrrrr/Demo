ad383fa754d81f989981dc2edb5725049f1e7131
Abi <abikrdubey@gmail.com>
2023-12-03 11:57:13 +0530

    hey

--- com/springcore/constructor/Test.java
+++ com/springcore/constructor/Test.java
@@ -8,9 +8,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	ApplicationContext context =new ClassPathXmlApplicationContext("com/springcore/constructor/constructor.xml");
-	Person c = (Person) context.getBean("Person1");
+	Person c = (Person) context.getBean("person1");
 	
 	System.out.println(c);
+	System.out.println("hello");
 	}
 
 }
